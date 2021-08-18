799657c3efdb4a9c4005341c9130c3ae333b8e78
PetkoMarinov <p.m.marinov@abv.bg>
2021-08-19 00:58:22 +0300

    Exercise

---
+++ Exams/Fundamentals Final 04.04.2020/ActivationKeys/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace ActivationKeys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ActivationKey activationKey = new ActivationKey(Console.ReadLine());
+            string instructions = Console.ReadLine();
+
+            while (instructions != "Generate")
+            {
+                string[] instructionsData = instructions.Split(">>>");
+                string command = instructionsData[0];
+
+                switch (command)
+                {
+                    case "Contains":
+                        string substring = instructionsData[1];
+                        activationKey.Contains(substring); break;
+
+                    case "Flip":
+                        string upLowCommand = instructionsData[1];
+                        int flipStartIndex = int.Parse(instructionsData[2]);
+                        int flipEndIndex = int.Parse(instructionsData[3]);
+                        activationKey.Flip(upLowCommand, flipStartIndex, flipEndIndex);
+                        Print(activationKey.Text); break;
+
+                    case "Slice":
+                        int sliceStartIndex = int.Parse(instructionsData[1]);
+                        int sliceEndIndex = int.Parse(instructionsData[2]);
+                        activationKey.Slice(sliceStartIndex, sliceEndIndex);
+                        Print(activationKey.Text); break;
+                }
+
+                instructions = Console.ReadLine();
+            }
+
+            Print($"Your activation key is: {activationKey.Text}");
+        }
+        public static void Print(string text) => Console.WriteLine(text);
+    }
+
+    public class ActivationKey
+    {
+        public ActivationKey(string text)
+        {
+            Text = text;
+        }
+        public string Text { get; set; }
+
+        public void Contains(string substring)
+        {
+            if (this.Text.Contains(substring))
+            {
+                Console.WriteLine($"{this.Text} contains {substring}");
+            }
+            else
+            {
+                Console.WriteLine("Substring not found!");
+            }
+        }
+
+        public string Flip(string command, int startIndex, int endIndex)
+        {
+            string flip = command == "Upper"
+                    ? this.Text.Substring(startIndex, endIndex - startIndex).ToUpper()
+                    : this.Text.Substring(startIndex, endIndex - startIndex).ToLower();
+
+            this.Text = this.Text.Remove(startIndex, endIndex - startIndex);
+            this.Text = this.Text.Insert(startIndex, flip);
+            return this.Text;
+        }
+
+        public string Slice(int startIndex, int endIndex)
+        {
+            this.Text = this.Text.Remove(startIndex, endIndex - startIndex);
+            return this.Text;
+        }
+    }
+}
---
+++ Exams/Fundamentals Final 04.04.2020/P!rates/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P_rates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<City> cities = new List<City>();
+            string input = Console.ReadLine();
+
+            while (input != "Sail")
+            {
+                string[] targetCity = input.Split("||");
+                string name = targetCity[0];
+                int population = int.Parse(targetCity[1]);
+                int gold = int.Parse(targetCity[2]);
+
+                if (cities.Any(c=>c.Name==name))
+                {
+                    var city = cities.Find(x => x.Name == name);
+                    city.Population += population;
+                    city.Gold += gold;
+                }
+                else
+                {
+                    cities.Add(new City(name, population, gold));
+                }
+
+                input = Console.ReadLine();
+            }
+
+            string events = Console.ReadLine();
+            while (events != "End")
+            {
+                string[] eventsData = events.Split("=>");
+                string command = eventsData[0];
+                string townName = eventsData[1];
+
+                if (command == "Plunder")
+                {
+                    int peopleKilled = int.Parse(eventsData[2]);
+                    int plunderedGold = int.Parse(eventsData[3]);
+                    var plunderedCity = cities.Find(x => x.Name == townName);
+                    plunderedCity.Gold -= plunderedGold;
+                    plunderedCity.Population -= peopleKilled;
+
+                    Console.WriteLine($"{townName} plundered! {plunderedGold} gold stolen," +
+                        $" {peopleKilled} citizens killed.");
+
+                    if (plunderedCity.Gold <= 0 || plunderedCity.Population <= 0)
+                    {
+                        cities.Remove(plunderedCity);
+                        Console.WriteLine($"{plunderedCity.Name} has been wiped off the map!");
+                    }
+                }
+                else if (command == "Prosper")
+                {
+                    var prosperCity = cities.Find(x => x.Name == townName);
+                    int prosperGold = int.Parse(eventsData[2]);
+
+                    if (prosperGold < 0)
+                    {
+                        Console.WriteLine("Gold added cannot be a negative number!");
+                    }
+                    else
+                    {
+                        prosperCity.Gold += prosperGold;
+                        Console.WriteLine($"{prosperGold} gold added to the city treasury." +
+                            $" {townName} now has {prosperCity.Gold} gold.");
+                    }
+                }
+
+                events = Console.ReadLine();
+            }
+
+            if (cities.Count > 0)
+            {
+                Console.WriteLine($"Ahoy, Captain! There are {cities.Count} wealthy" +
+                    $" settlements to go to:");
+
+                cities = cities.OrderByDescending(x => x.Gold).ThenBy(x => x.Name).ToList();
+                
+                foreach (var city in cities)
+                {
+                    Console.WriteLine($"{city.Name} -> Population: {city.Population}" +
+                        $" citizens, Gold: {city.Gold} kg");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ahoy, Captain! All targets have been plundered" +
+                    " and destroyed!");
+            }
+        }
+    }
+
+    class City
+    {
+        public City(string name, int population, int gold)
+        {
+            Name = name;
+            Population = population;
+            Gold = gold;
+        }
+
+        public string Name { get; }
+        public int Population { get; set; }
+        public int Gold { get; set; }
+
+        public void Prosper(int gold) => this.Gold += gold;
+    }
+}

38f069bb56ec1616e124a4e2d78358ca939bedce
PetkoMarinov <p.m.marinov@abv.bg>
2021-08-19 22:14:16 +0300

    Exercise

---
+++ Exams/Fundamentals Final 04.04.2020/Password Reset/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+
+namespace PasswordReset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder pass = new StringBuilder(Console.ReadLine());
+            string input = Console.ReadLine();
+
+            while (input != "Done")
+            {
+                string[] instruction = input.Split();
+                string command = instruction[0];
+
+                switch (command)
+                {
+                    case "TakeOdd":
+                        StringBuilder temp = new StringBuilder();
+                        for (int i = 1; i < pass.Length; i += 2)
+                        {
+                            temp.Append(pass[i]);
+                        }
+                        pass = temp;
+                        Console.WriteLine(pass); break;
+
+                    case "Cut":
+                        int index = int.Parse(instruction[1]);
+                        int length = int.Parse(instruction[2]);
+                        pass.Remove(index, length);
+                        Console.WriteLine(pass); break;
+
+                    case "Substitute":
+                        string substring = instruction[1];
+                        string substitute = instruction[2];
+
+                        if (pass.ToString().Contains(substring))
+                        {
+                            pass.Replace(substring, substitute);
+                            Console.WriteLine(pass);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Nothing to replace!");
+                        }
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Your password is: {pass.ToString()}");
+        }
+    }
+}
---
+++ Exams/Fundamentals Retake 15.08.2020/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdAstra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Regex regex = new Regex(@"([|#])([A-Za-z\s]+)\1(\d{2}\/\d{2}\/\d{2})\1(\d{1,5})\1");
+            MatchCollection foodInfo = regex.Matches(input);
+            
+            int totalCalories = foodInfo.Sum(x=>int.Parse(x.Groups[4].Value));
+            int days = totalCalories / 2000;
+
+            Console.WriteLine($"You have food to last you for: {days} days!");
+            
+            foreach (Match item in foodInfo)
+            {
+                string name = item.Groups[2].Value;
+                string expexpirationDate= item.Groups[3].Value;
+                string calories = item.Groups[4].Value;
+
+                Console.WriteLine($"Item: {name}, Best before: {expexpirationDate}," +
+                    $" Nutrition: {calories}");
+            }
+        }
+    }
+}

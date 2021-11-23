f628eec1d631a0155503e941af22a19017d3b306
PetkoMarinov <p.m.marinov@abv.bg>
2021-11-23 23:27:04 +0200

    Exercise

--- C# EF Core/JSON Processing/CarDealer/CarDealerProfile.cs
+++ C# EF Core/JSON Processing/CarDealer/CarDealerProfile.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using AutoMapper;
-using CarDealer.Models;
-
-namespace CarDealer
+﻿namespace CarDealer
 {
+    using AutoMapper;
+    using CarDealer.DTO;
+    using CarDealer.DTO.Import;
+    using CarDealer.Models;
+
     public class CarDealerProfile : Profile
     {
         public CarDealerProfile()
         {
-        
+            this.CreateMap<DtoSupplier, Supplier>();
+            this.CreateMap<DtoParts, Part>();
         }
     }
 }
--- C# EF Core/JSON Processing/CarDealer/StartUp.cs
+++ C# EF Core/JSON Processing/CarDealer/StartUp.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Linq;
 using AutoMapper;
 using CarDealer.Data;
+using CarDealer.DTO;
+using CarDealer.DTO.Import;
 using CarDealer.Models;
 using Newtonsoft.Json;
 
@@ -11,8 +13,43 @@ namespace CarDealer
 {
     public class StartUp
     {
+        private static IMapper mapper;
         public static void Main(string[] args)
         {
+            var context = new CarDealerContext();
+            //context.Database.EnsureDeleted();
+            //context.Database.EnsureCreated();
+
+            string inputSuppliers = File.ReadAllText(@"E:\C#\C# EF Core\JSON Processing\CarDealer\Datasets\suppliers.json");
+            string inputParts = File.ReadAllText(@"E:\C#\C# EF Core\JSON Processing\CarDealer\Datasets\parts.json");
+            Console.WriteLine(ImportSuppliers(context, inputSuppliers));
+        }
+
+        public static string ImportParts(CarDealerContext context, string inputJson)
+        {
+            InitializeMapper();
+            var dtoParts = JsonConvert.DeserializeObject<IEnumerable<DtoParts>>(inputJson);
+            var 
+            return $"Successfully imported {Parts.Count}.";
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            InitializeMapper();
+            var dtoSupplers = JsonConvert.DeserializeObject<IEnumerable<DtoSupplier>>(inputJson);
+
+            var suppliers = mapper.Map<IEnumerable<Supplier>>(dtoSupplers);
+
+            context.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Count()}.";
+        }
+
+        public static void InitializeMapper()
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<CarDealerProfile>());
+            mapper = new Mapper(config);
         }
     }
 }
